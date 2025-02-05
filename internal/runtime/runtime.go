36217f9bdf48f89b589476e2cad9e77e45832064
Valentin Deaconu <valentin.deaconu@sectorlabs.ro>
2025-02-05 15:42:53 +0200

    Document the runtime interface

--- internal/runtime/runtime.go
+++ internal/runtime/runtime.go
@@ -1,8 +1,15 @@
 package runtime
 
-// Runtime TODO
+// Runtime is an abstract interface that represents the behavior
+// of a func runtime.
 type Runtime interface {
+	// Parse handles the source parsing.
+	//
+	// A runtime can be called multiple time to parse different
+	// (or even the same) sources. The runtime should handle the
+	// overrides and make sure repetitive calls are allowed.
 	Parse(src string) error
 
+	// Functions returns a slice of Terraform-compatible functions.
 	Functions() []Function
 }
